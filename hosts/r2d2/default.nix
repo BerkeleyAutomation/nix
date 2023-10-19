71100a995fe04ab324490c02a29026a53c32e077
Oliver Ni <oliver.ni@gmail.com>
2023-10-18 22:14:31 -0700

    Add kanidm

--- hosts/r2d2/default.nix
+++ hosts/r2d2/default.nix
@@ -5,24 +5,9 @@
     ./hardware.nix
     ./networking.nix
     ../../profiles/docker.nix
+    ../../profiles/kanidm
   ];
 
   networking.hostName = "r2d2";
   system.stateVersion = "23.05";
-
-  age.secrets.cf-cert-creds.file = ../../secrets/cf-cert-creds.age;
-
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "oliverni@berkeley.edu";
-
-    certs."idm.berkeleyautomation.net" = {
-      dnsProvider = "cloudflare";
-      credentialsFile = config.age.secrets.cf-cert-creds.path;
-    };
-  };
-
-  virtualisation.oci-containers = {
-    backend = "docker";
-  };
 }
---
+++ profiles/kanidm/default.nix
@@ -0,0 +1,33 @@
+{ config, ... }:
+
+{
+  environment.etc = {
+    "kanidm/server.toml".source = ./server.toml;
+  };
+
+  age.secrets.cf-cert-creds.file = ../../secrets/cf-cert-creds.age;
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "oliverni@berkeley.edu";
+
+    certs."idm.berkeleyautomation.net" = {
+      dnsProvider = "cloudflare";
+      credentialsFile = config.age.secrets.cf-cert-creds.path;
+    };
+  };
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+
+    containers.kanidm-server = {
+      ports = [ "443:8443" "636:3636" ];
+      image = "kanidm/server:latest";
+      volumes = [
+        "/var/lib/acme/idm.berkeleyautomation.net:/tls"
+        "/var/lib/kanidm:/data"
+        "/etc/kanidm/server.toml:/data/server.toml"
+      ];
+    };
+  };
+}
