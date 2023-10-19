57a3481232344c6bd970d9f717aa5583b8beab3f
Oliver Ni <oliver.ni@gmail.com>
2023-10-19 01:28:08 -0700

    Update nixpkgs to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Configuration for AUTOLab";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/23.05;
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
     flake-utils-plus.url = github:gytis-ivaskevicius/flake-utils-plus/v1.4.0;
     agenix.url = "github:ryantm/agenix/0.14.0";
   };
