a1eb60774670ff634621ba1f563a024d9afb4c08
Oliver Ni <oliver.ni@gmail.com>
2024-04-11 23:41:56 -0700

    fix: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,8 @@ let
   users = [ oliver ];
 
   bumblebee = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN1iq0eveXStUo2wqGMHEtvw41z0F7fwM13BXafjVxzc";
-  hosts = [ bumblebee ];
+  nixos-test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOI2/HHHOpmrGtFWFJCV8EJNqstMqL+gOcEI6XX2fPan";
+  hosts = [ bumblebee nixos-test ];
 in
 {
   "cf-cert-creds.age".publicKeys = users ++ hosts;
